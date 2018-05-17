793a6d43a142499546345c0a7033ab378d77e0f2
phenom_hunaif_muhammed <hunaif.muhammed@phenompeople.com>
2018-05-17 15:11:46 +0530

    added a basic model to movie sentiment classification

---
+++ src/movie_sentiment_classsification/movie_sentiment_classification.py
@@ -0,0 +1,82 @@
+from keras.datasets import imdb
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(
+    num_words=10000)
+
+def retrieve_review(sentence):
+    word_index = imdb.get_word_index()
+    reverse_word_index = dict(
+        [(value, key) for (key, value) in word_index.items()])
+    decoded_review = ' '.join(
+        [reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+    return  decoded_review
+
+
+
+import numpy as np
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+all_history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+history_dict = all_history.history
+print(history_dict.keys())
+
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+
+epochs = range(1, len(loss_values) + 1)
+
+# plt.plot(epochs, loss_values, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+# plt.show()
+
+
+plt.clf()
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+plt.plot(epochs, acc_values, 'bo', label='Training acc')
+plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
